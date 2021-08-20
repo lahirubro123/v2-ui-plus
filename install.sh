6abf89f067382974ef51ead00f7cde52ec5b93f6
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2021-08-20 20:36:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,20 +15,23 @@ mv v2-ui-plus /usr/local/sbin/v2-ui-plus
 cd /usr/local/sbin/v2-ui-plus
 pip3 install -r requirements.txt
 
-# Install snap packages
-tmux 
-cd /usr/local/sbin/v2-ui-plus
+# Install  packages
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get install certbot
 
-
- 
 # install certificate
 echo Hello, what is your e mail address ?
 read email
 echo what is your doamin ?
 read domain
 sudo certbot certonly --standalone --preferred-challenges http --agree-tos --email $email -d $domain 
+echo please save this text and type any key and press enter
+read -r -s -p $'Press enter to continue...'
+
+# open tmux session
+tmux new-session -d -s "myTempSession" /usr/local/sbin/v2-ui-plus python3 v2-ui-plus.py
+
+
 
 
