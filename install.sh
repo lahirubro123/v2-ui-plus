bf761b2f806494109fc8314cb9604f22ef991269
Lahiru Sandaruwan Liyanage <72745298+lahirubro123@users.noreply.github.com>
2021-08-21 10:27:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,32 +16,24 @@ mv v2-ui-plus /usr/local/sbin/v2-ui-plus
 cd /usr/local/sbin/v2-ui-plus
 pip3 install -r requirements.txt
 
-
-
-
-# open tmux session
-cd /usr/local/sbin/v2-ui-plus
-tmux new-session -d -s v2ray python3 v2-ui.py
-echo please wait adding cronjob
-
-echo panel  your_ip:65432
-echo Thank You.....!! (Bunny_lk)
-read -r -s -p $'Press enter to continue...'
-
 # Install  packages
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get install certbot
-
 # install certificate
 echo Hello, what is your e mail address ?
 read email
 echo what is your doamin ?
 read domain
-
 sudo certbot certonly --standalone --preferred-challenges http --agree-tos --email $email -d $domain 
-read -r -s -p $'Press enter to continue...'
 (crontab -l; echo "30 0 * * * 0 12 * * * /usr/bin/certbot renew --quiet") | sort -u | crontab -
+
+# open tmux session
+cd /usr/local/sbin/v2-ui-plus
+tmux new-session -d -s v2ray python3 v2-ui.py
+echo panel  your_ip:65432
+echo Thank You.....!! (Bunny_lk)
+read -r -s -p $'Press enter to continue...'
 
 echo thank you
 
